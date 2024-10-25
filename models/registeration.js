7382ea7923afe2df067177e249dc12d3f01cfdde
Hamdan_Vohra <hamdanvohra5676@gmail.com>
2024-10-25 16:13:27 +0500

    Update Registeration table name

--- models/registeration.js
+++ models/registeration.js
@@ -10,13 +10,13 @@ const db = mysql.createPool({
   queueLimit: 0,
 });
 
-const registerationSchema = `CREATE TABLE IF NOT EXISTS Registeration(
+const registerationSchema = `CREATE TABLE IF NOT EXISTS Registerations(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL ,
+    userName VARCHAR(255) NOT NULL ,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE,
     role VARCHAR(10) CHECK (role IN ('student','teacher')),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )`;
 
 db.query(registerationSchema, (err, result) => {
