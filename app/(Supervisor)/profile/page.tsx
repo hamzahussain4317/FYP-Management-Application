45e6a5001d61b6544c1a940d89d31a1f7be77bd0
Hamza Hussain <140734595+hamzahussain4317@users.noreply.github.com>
2024-12-07 22:24:48 +0500

    Supervisor Profile Integration Complete

--- app/(Supervisor)/profile/page.tsx
+++ app/(Supervisor)/profile/page.tsx
@@ -21,8 +21,7 @@ import { useSupervisorContext } from "@/context/SupervisorContext";
 //   },
 // };
 const Profile = () => {
-  const { profile, fetchProfile } = useSupervisorContext();
-  const { personalInfo, academicInfo, projectDetails } = profile;
+  const { profile, fetchProfile,isLoading } = useSupervisorContext();
 
   useEffect(() => {
     const userId = sessionStorage.getItem("userId");
@@ -33,73 +32,85 @@ const Profile = () => {
       <h1 className="text-4xl font-bold text-center text-gray-800 mb-8">
         Supervisor | Profile
       </h1>
+      {!isLoading && profile !== undefined ?
+      (
+        <>
+          <div className="mb-10 bg-white p-6 rounded-lg shadow-md bg-[#f9f9f9] min-h-[30vh] flex flex-col">
+            <h2 className="text-2xl font-semibold text-gray-700 mb-4">
+              Personal Information
+            </h2>
+            <ul className="space-y-4 text-gray-600 grid grid-cols-1 md:grid-cols-[1fr_1fr] gap-0 min-w-full flex-grow">
+              <li className="mt-4">
+                <strong>Name:</strong> {`${profile.personalInfo.name || "None"}`}
+              </li>
+              <li>
+                <strong>Email:</strong> {`${profile.personalInfo.email || "None"}`}
+              </li>
+              <li>
+                <strong>Contact Number:</strong>{" "}
+                {`${profile.personalInfo.contactNumber || "None"}`}
+              </li>
+              <li>
+                <strong>Department:</strong> {`${profile.personalInfo.department || "None"}`}
+              </li>
+              <button className="mt-auto col-span-2 text-center bg-green-500 text-white py-2 px-4 rounded hover:bg-blue-600">
+                <Link href="/edit-profile">Edit</Link>
+              </button>
+            </ul>
+          </div>
 
-      {/* Personal Information Section */}
-      <div className="mb-10 bg-white p-6 rounded-lg shadow-md bg-[#f9f9f9] min-h-[30vh] flex flex-col">
-        <h2 className="text-2xl font-semibold text-gray-700 mb-4">
-          Personal Information
-        </h2>
-        <ul className="space-y-4 text-gray-600 grid grid-cols-1 md:grid-cols-[1fr_1fr] gap-0 min-w-full flex-grow">
-          <li className="mt-4">
-            <strong>Name:</strong> {personalInfo.name}
-          </li>
-          <li>
-            <strong>Email:</strong> {personalInfo.email}
-          </li>
-          <li>
-            <strong>Contact Number:</strong> {personalInfo.contactNumber}
-          </li>
-          <li>
-            <strong>Department:</strong> {personalInfo.department}
-          </li>
-          <button className="mt-auto col-span-2 text-center bg-green-500 text-white py-2 px-4 rounded hover:bg-blue-600">
-            <Link href="/edit-profile">Edit</Link>
-          </button>
-        </ul>
-      </div>
+          <div className="mb-10 bg-white p-6 rounded-lg shadow-md bg-[#f9f9f9] min-h-[30vh] flex flex-col">
+            <h2 className="text-2xl font-semibold text-gray-700 mb-4">
+              Academic Information
+            </h2>
+            <ul className="space-y-4 text-gray-600 grid grid-cols-1 md:grid-cols-[1fr_1fr] gap-0 min-w-full flex-grow">
+              <li className="mt-4">
+                <strong>Domain:</strong> {`${profile.academicInfo.domain || "None"}`}
+              </li>
+              <li>
+                <strong>CGPA Criteria:</strong>{" "}
+                {`${profile.academicInfo.cgpaCriteria || "None"}`}
+              </li>
+              <li>
+                <strong>Qualification:</strong>{" "}
+                 {`${profile.academicInfo.qualification || "None"}`}
+              </li>
+              <li>
+                <strong>Designation:</strong>{" "}
+                {`${profile.academicInfo.designation || "None"}`}
+              </li>
+              <button className="mt-auto col-span-2 text-center bg-green-500 text-white py-2 px-4 rounded hover:bg-blue-600">
+                <Link href="/edit-profile">Edit</Link>
+              </button>
+            </ul>
+          </div>
 
-      <div className="mb-10 bg-white p-6 rounded-lg shadow-md bg-[#f9f9f9] min-h-[30vh] flex flex-col">
-        <h2 className="text-2xl font-semibold text-gray-700 mb-4">
-          Academic Information
-        </h2>
-        <ul className="space-y-4 text-gray-600 grid grid-cols-1 md:grid-cols-[1fr_1fr] gap-0 min-w-full flex-grow">
-          <li className="mt-4">
-            <strong>Domain:</strong> {academicInfo.domain}
-          </li>
-          <li>
-            <strong>CGPA Criteria:</strong> {academicInfo.cgpaCriteria}
-          </li>
-          <li>
-            <strong>Experience:</strong> {academicInfo.experience}
-          </li>
-          <button className="mt-auto col-span-2 text-center bg-green-500 text-white py-2 px-4 rounded hover:bg-blue-600">
-            <Link href="/edit-profile">Edit</Link>
-          </button>
-        </ul>
-      </div>
-
-      <div className="bg-white p-6 rounded-lg shadow-md bg-[#f9f9f9] min-h-[30vh] flex flex-col">
-        <h2 className="text-2xl font-semibold text-gray-700 mb-4">
-          Project Details
-        </h2>
-        <ul className="space-y-4 text-gray-600 grid grid-cols-1 md:grid-cols-[1fr_1fr] gap-0 min-w-full flex-grow">
-          <li className="mt-4">
-            <strong>Currently Handling Projects:</strong>{" "}
-            {projectDetails.currentProjects}
-          </li>
-          {/* <li>
+          <div className="bg-white p-6 rounded-lg shadow-md bg-[#f9f9f9] min-h-[30vh] flex flex-col">
+            <h2 className="text-2xl font-semibold text-gray-700 mb-4">
+              Project Details
+            </h2>
+            <ul className="space-y-4 text-gray-600 grid grid-cols-1 md:grid-cols-[1fr_1fr] gap-0 min-w-full flex-grow">
+              <li className="mt-4">
+                <strong>Currently Handling Projects:</strong>{" "}
+                {`${profile.projectDetails.currentProjects || "None"}`}
+              </li>
+              {/* <li>
             <strong>Completed Projects:</strong>{" "}
             {projectDetails.completedProjects}
-          </li>
-          <li>
+            </li>
+            <li>
             <strong>Supervised Projects:</strong>{" "}
             {projectDetails.supervisedProjects}
-          </li>
-          <li>
+            </li>
+            <li>
             <strong>Groups Handled:</strong> {projectDetails.groupsHandled}
-          </li> */}
-        </ul>
-      </div>
+            </li> */}
+            </ul>
+          </div>
+        </>):(
+          <div>Loading....</div>
+        )
+      }
     </section>
   );
 };
