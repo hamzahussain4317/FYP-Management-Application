797ef2cb612d1efa5caa5f0f8275cd3cdf776c00
Hamdan_Vohra <hamdanvohra5676@gmail.com>
2024-12-09 10:51:25 +0500

    Socket user regsitered

--- context/AppDataContext.tsx
+++ context/AppDataContext.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { useContext, createContext, useState } from "react";
-
+import { useContext, createContext, useState, useEffect } from "react";
+import socket from "@/utils/socket";
 const AppDataContext = createContext<any>(undefined);
 
 export default function AppWrapper({
@@ -9,11 +9,25 @@ export default function AppWrapper({
   children: React.ReactNode;
 }) {
   const [userId, setUserId] = useState<number>();
-  const [userName , setUserName]=useState<string>();
-  const [profilePic , setProfilePic]=useState<string>();
+  const [userName, setUserName] = useState<string>();
+  const [profilePic, setProfilePic] = useState<string>();
+  const [socketState, setSocketState] = useState<any>(undefined);
+  const [notifications, setNotifications] = useState<Notifications[]>();
+
+  useEffect(() => {
+    setSocketState(socket);
+  }, []);
   return (
     <AppDataContext.Provider
-      value={{ userId, setUserId , userName , setUserName , profilePic , setProfilePic}}
+      value={{
+        userId,
+        setUserId,
+        userName,
+        setUserName,
+        profilePic,
+        setProfilePic,
+        socketState,
+      }}
     >
       {children}
     </AppDataContext.Provider>
