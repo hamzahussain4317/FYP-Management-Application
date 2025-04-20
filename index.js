adf99f6e63d3ed07cd7bcd35e9b36f19f1c44179
Hamdan_Vohra <hamdanvohra5676@gmail.com>
2025-04-20 20:04:10 +0500

    Remove Unnecessary Connections

--- index.js
+++ index.js
@@ -8,31 +8,6 @@ const { Server } = require("socket.io");
 const dbPool = require("./db/pool.js");
 const { isValidToken } = require("./middlewares/validations.js");
 
-// db.js
-
-
-const client = new Client({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  connectionString: process.env.DATABASE_URL,
-});
-
-client.connect()
-  .then(() => {
-    console.log('✅ Connected to Supabase PostgreSQL successfully!');
-  })
-  .catch((err) => {
-    console.error('❌ Connection error:', err.stack);
-  });
-
-
-
 const auth = require("./routes/auth");
 const student = require("./routes/student");
 const supervisor = require("./routes/supervisor");
